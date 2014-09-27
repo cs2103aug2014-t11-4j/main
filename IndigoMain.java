6544ae416cfacfb9addba69ed3cf6568bce1a544
jiajielu <lujiajie@nus.edu.sg>
2014-09-28 02:15:12 +0800

    MainIndigo
    A main program of java Indigo.

---
+++ IndigoMain.java
@@ -0,0 +1,24 @@
+/** This a main program of Indigo.
+ *  Indigo is a software that can store, process and display tasks on the desktop as a task manager.
+ *  Indigo also supports audio remainder, auto-correction, and color-coded highlighting.
+ *  Indigo takes input from keyboard only.
+ *  Indigo stores data on a local disk.
+ * @author jjlu
+ *
+ */
+public class IndigoMain {
+
+	public static void main(String[] args) {
+		/* outline:
+		 * 1.welcome message
+		 * 2.check if there is local disk storage
+		 * -yes load data
+		 * -no display beginner tutorial message
+		 * 3.asks for user command
+		 * 4.process user command
+		 * 5.repeating 3-4 until exit
+		 */
+
+	}
+
+}
