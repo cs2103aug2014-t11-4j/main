54aef48507122e37ec5c409f00e3c8554d39b119
jiajielu <lujiajie@nus.edu.sg>
2014-09-28 02:34:56 +0800

    template
    creating method template for methods in main.

--- IndigoMain.java
+++ IndigoMain.java
@@ -19,6 +19,44 @@ public class IndigoMain {
 		 * 5.repeating 3-4 until exit
 		 */
 
+		displayWelcomeMessage();
+		
+		if (isDataPresent()){
+			loadData();
+		} else {
+			displayTutorialMessage();
+		}
+		
+		while (true){
+			readCommand();
+		} 
+	}
+
+	private static void readCommand() {
+		/* TODO read a command from user and process it
+		 * parser
+		 * execute Command
+		 */		
+	}
+
+	private static void displayTutorialMessage() {
+		// TODO ask user if want to enter tutorial mode
+		
+	}
+
+	private static void loadData() {
+		// TODO load data from the local disk into memory
+		
+	}
+
+	private static boolean isDataPresent() {
+		// TODO check if there is data on the disk
+		return false;
+	}
+
+	private static void displayWelcomeMessage() {
+		// TODO display welcomeMessage
+		
 	}
 
 }
