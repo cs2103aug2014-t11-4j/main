8a16102332ef407c8e235d5bc6b5a093868dcb05
jiajielu <lujiajie@nus.edu.sg>
2014-10-03 14:18:18 +0800

    create a toString method for Task class

--- Task.java
+++ Task.java
@@ -1,98 +1,104 @@
-/** this class is meant for storage of one task only
- * 3 way to create a task
- * -task(Parser)
- * -task()
- * -task(String, Type, String, String, String)
- * There are 5 fields for creating a new task: the title, the description, the time, the type of the task and the level of importance.
+/**
+ * this class is meant for storage of one task only 3 way to create a task
+ * -task(Parser) -task() -task(String, Type, String, String, String) There are 5
+ * fields for creating a new task: the title, the description, the time, the
+ * type of the task and the level of importance.
+ * 
  * @author jjlu
  *
  */
 
 public class Task {
-	
-	private String taskTitle;
-	
+
+	private String taskTitle; // without time
+
 	private Type taskType;
-	
-	private String taskDescription;
-	
-	//TODO data type to be amended
+
+	private String taskDescription; // with time
+
+	// TODO data type to be amended
 	private int[] taskTime;
-	// exact time period, date, week or month
-	
+	// exact time period, date, week or month in the format of DD/MM/YY
+
 	private String taskImportance;
+
 	// important, immediate, normal or pleasure
-	
+
 	public enum Type {
-		FLOATING, DAILY, WEEKLY
+		FLOATING, TIMED, DEADLINE
 	}
 
 	public Task() {
 		// a stub default value for default constructor
-		this ("CS2103", Type.FLOATING, "finish CS2103 assignments", null, "immediate");
+		this("CS2103", Type.FLOATING, "finish CS2103 assignments", null,
+				"immediate");
 	}
-	
+
 	// TODO exact class method to be ammended
-	public Task(Parser userCommand){
-		this (userCommand.getCommand(), Type.FLOATING, "Enter your task description", userCommand.getDate(), "immediate");
+	public Task(Parser userCommand) {
+		this(userCommand.getCommand(), Type.FLOATING,
+				"Enter your task description", userCommand.getDate(),
+				"immediate");
 	}
-	
-	public Task(String title, Type type, String description, int[] time, String importance){
+
+	public Task(String title, Type type, String description, int[] time,
+			String importance) {
 		taskTitle = title;
 		taskType = type;
 		taskDescription = description;
 		taskTime = time;
 		taskImportance = importance;
 	}
-	
-	public Task(String taskDescription){
+
+	// floating task
+	public Task(String taskDescription) {
 		this("title", Type.FLOATING, taskDescription, null, "immediate");
 	}
-	
+
 	// accessor
-	public String getTitle(){
+	public String getTitle() {
 		return taskTitle;
 	}
-	
-	public Type getType(){
+
+	public Type getType() {
 		return taskType;
 	}
-	
-	public String getDescription(){
+
+	public String getDescription() {
 		return taskDescription;
 	}
-	
-	public int[] getTime(){
+
+	public int[] getTime() {
 		return taskTime;
 	}
-	
-	public String getImportance(){
+
+	public String getImportance() {
 		return taskImportance;
 	}
-	
+
 	// mutator
-	public String editTitle(String newTitle){
+	public String editTitle(String newTitle) {
 		taskTitle = newTitle;
 		return newTitle;
 	}
-	
-	public String editDescription (String newDescription){ 
+
+	public String editDescription(String newDescription) {
 		taskDescription = newDescription;
 		return newDescription;
 	}
-	
-	public int[] editTime (int[] newTime){
+
+	public int[] editTime(int[] newTime) {
 		taskTime = newTime;
 		return newTime;
 	}
-	
-	public String editImportance (String newImportance){
+
+	public String editImportance(String newImportance) {
 		taskImportance = newImportance;
 		return newImportance;
 	}
-	
+
 	// TODO support floating task only
-	public String toString(){
+	public String toString() {
 		return this.getDescription();
 	}
 
