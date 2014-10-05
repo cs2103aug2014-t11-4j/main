6e8e71caae34acf4cda93e77c89647d05b6a55b1
sritampatnaik <sritampatnaik@gmail.com>
2014-10-05 15:28:41 +0800

    Create InputWindow.java

---
+++ InputWindow.java
@@ -0,0 +1,76 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+
+public class InputWindow  extends JFrame implements ActionListener{
+	
+	JPanel welcomeWindow = new JPanel(new BorderLayout());
+	JTextField readInput = new JTextField();
+	static JTextArea textArea = new JTextArea("",5, 20);
+	public static String userCommand;
+	public static final ArrayList<String> inputs = new ArrayList<String>();
+	
+	public static void main(String[] args){
+		
+		inputs.add("call mom");
+		inputs.add("catch a dog");
+		inputs.add("catch a fish");
+		
+		JFrame welcome = new InputWindow();
+		welcome.setBounds(400, 200, 450, 300);
+		welcome.setVisible(true);
+		welcome.setBackground(Color.white);
+	}
+	
+	public InputWindow(){
+		
+		super("Indigo");
+		setResizable(true);
+		welcomeWindow.setBorder(new EmptyBorder(20,20,20,20));
+
+		welcomeWindow.add(readInput, BorderLayout.NORTH);
+		welcomeWindow.add(textArea, BorderLayout.CENTER);
+		add(welcomeWindow);
+		
+		readInput.addActionListener(this);
+		
+		setVisible(true);
+	}
+	
+	
+	public static void showFeedback() {
+		textArea.append("added task: "+ userCommand + "\n");	
+	}
+	public static void showTaskList(){
+		for (int i=0; i < inputs.size(); i++){
+			textArea.append(inputs.get(i) + '\n');	
+		}
+			
+	}
+	public static String getUserCommand(){
+		return userCommand;
+	}
+	
+	
+	public void actionPerformed(ActionEvent evt) {
+		 	String text = readInput.getText();
+	        readInput.selectAll();
+	        setUserCommand(text);
+	        textArea.setText("");
+	        showFeedback();
+	        showTaskList();
+	        
+	    }
+
+	private void setUserCommand(String text) {
+		userCommand = text;
+		
+	}
