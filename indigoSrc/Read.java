e45b4361a42bed2b846d13529f62a5a9562d2e2e
kenhua-l <l_kenhua93@hotmail.com>
2014-10-23 02:34:32 +0800

    put test cases on read

--- indigoSrc/Read.java
+++ indigoSrc/Read.java
@@ -10,6 +10,7 @@ public class Read extends CommandClass{
 	
 	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss");
 	private static final String FILE_NAME = "myTask";
+	static final String newLine = System.getProperty("line.separator");
 
 	@Override
 	public String execute() {
@@ -41,7 +42,7 @@ public class Read extends CommandClass{
 		} else if (parserVar.getCommand().contains("-t")){
 			return taskListVar.viewTimedTask(DATE_FORMAT);
 		} else if (parserVar.getCommand().contains("today")){
-			return viewToday();
+			return viewToday(taskListVar);
 		} else
 			return taskListVar.viewAll(DATE_FORMAT);
 	}
@@ -54,34 +55,42 @@ public class Read extends CommandClass{
 		return taskListVar.viewUndone();
 	}
 	
-/*	public static void main(String args[]){
-		TaskList ntl = new TaskList();
-		DeadlineTask time = new DeadlineTask("boo", new DateTime(2014,10,22,19,15,00));
-		DeadlineTask time1 = new DeadlineTask("bee", new DateTime(2014,10,24,19,15,00));
-		DeadlineTask time2 = new DeadlineTask("baa", new DateTime(2014,10,26,19,15,00));
-		ntl.addTask(time);
-		ntl.addTask(time2);
-		ntl.addTask(time1);
+	public static void main(String args[]){
+		TaskList test = new TaskList();
+		DeadlineTask time0 = new DeadlineTask("buy book", new DateTime(2014,10,23,11,00,00));
+		DeadlineTask time1 = new DeadlineTask("read book", new DateTime(2014,10,24,19,15,00));
+		DeadlineTask time2 = new DeadlineTask("buy bread", new DateTime(2014,10,22,19,15,00));
+		DeadlineTask time3 = new DeadlineTask("eat bread", new DateTime(2013,5,22,19,15,00));
+		DeadlineTask time4 = new DeadlineTask("buy movie ticket", new DateTime(2013,11,21,19,15,00));
+		DeadlineTask time5 = new DeadlineTask("watch movie", new DateTime(2015,1,22,19,15,00));
+		DeadlineTask time6 = new DeadlineTask("go shopping", new DateTime(2015,10,23,19,15,00));
+		test.addTask(time0);
+		test.addTask(time1);
+		test.addTask(time2);
+		test.addTask(time3);
+		test.addTask(time4);
+		test.addTask(time5);
+		test.addTask(time6);
 		
-		System.out.println(ntl.viewAll(DATE_FORMAT));
-		
-		System.out.println(viewToday());
-		System.out.println(time.getTime().getYear());
-		System.out.println(time.getTime().getDayOfYear());
-		System.out.println(time2.getTime().getDayOfYear());
+		//Test the viewToday method. Only "buy book" should appear.
+		System.out.println(viewToday(test));
+		//Test the viewThisWeek method. "buy book" and "read book" only should appear.
+		System.out.println(viewThisWeek(test));
+		//Test overdue view. "buy bread", "eat bread", "buy movie ticket" should appear.
+		System.out.println(viewOverDue(test));
 	}
-*/
-	public static String viewToday(){
+	
+	//This method is to find tasks which are due today.
+	public static String viewToday(TaskList test){
 		DateTime now = new DateTime();
 		int yearNow = now.getYear();
 		int dayNow = now.getDayOfYear();
 		
-		String newLine = System.getProperty("line.separator");
 		StringBuilder result = new StringBuilder("Today's tasks are:" + newLine);
-		int tlSize = taskListVar.getSize();
+		int tlSize = test.getSize();
 		for (int i=0, j=1; i<tlSize; i++){
-			if ((taskListVar.get(i+1) instanceof DeadlineTask) && !(taskListVar.get(i+1) instanceof TimedTask)){
-				DeadlineTask temp = (DeadlineTask) taskListVar.get(i+1);
+			if ((test.get(i+1) instanceof DeadlineTask) && !(test.get(i+1) instanceof TimedTask)){
+				DeadlineTask temp = (DeadlineTask) test.get(i+1);
 				if((temp.getTime().getYear() == yearNow) && (temp.getTime().getDayOfYear() == dayNow)){
 					result.append("[NO." + j++ + "]" + temp.toString(DATE_FORMAT) + newLine);
 				}
@@ -90,19 +99,21 @@ public class Read extends CommandClass{
 		return result.toString();
 	}
 	
-	public static String viewThisWeek(){
+	//This method is to find tasks which are due this week. 
+	//It does not follow the pattern on Monday to Sunday. It takes tasks of
+	//the next seven days.
+	public static String viewThisWeek(TaskList test){
 		DateTime now = new DateTime();
 		int yearNow = now.getYear();
 		int dayNow = now.getDayOfYear();
 		int day7 = dayNow + 1;
 		
-		String newLine = System.getProperty("line.separator");
-		StringBuilder result = new StringBuilder("Today's tasks are:" + newLine);
-		int tlSize = taskListVar.getSize();
+		StringBuilder result = new StringBuilder("This week's tasks are:" + newLine);
+		int tlSize = test.getSize();
 		for (int i=0, j=1; i<tlSize; i++){
-			if ((taskListVar.get(i+1) instanceof DeadlineTask) && 
-				!(taskListVar.get(i+1) instanceof TimedTask)){
-				DeadlineTask temp = (DeadlineTask) taskListVar.get(i+1);
+			if ((test.get(i+1) instanceof DeadlineTask) && 
+				!(test.get(i+1) instanceof TimedTask)){
+				DeadlineTask temp = (DeadlineTask) test.get(i+1);
 				DateTime tempDate = temp.getTime();
 				int tempDateDay = tempDate.getDayOfYear();
 				if((tempDate.getYear() == yearNow) && 
@@ -114,21 +125,17 @@ public class Read extends CommandClass{
 		return result.toString();
 	}
 	
-	public static String viewOverDue(){
+	//This method lets user see tasks which are overdue but not done
+	public static String viewOverDue(TaskList test){
 		DateTime now = new DateTime();
 		int yearNow = now.getYear();
 		int dayNow = now.getDayOfYear();
-
-		System.out.println(now.getYear());
-		System.out.println(now.getDayOfYear());
 		
-		String newLine = System.getProperty("line.separator");
-		StringBuilder result = new StringBuilder("Today's tasks are:" + newLine);
-		int ntlsize = taskListVar.getSize();
-		System.out.println(ntlsize);
+		StringBuilder result = new StringBuilder("Tasks overdue are:" + newLine);
+		int ntlsize = test.getSize();
 		for (int i=0, j=1; i<ntlsize; i++){
-			if ((taskListVar.get(i+1) instanceof DeadlineTask) && !(taskListVar.get(i+1) instanceof TimedTask)){
-				DeadlineTask temp = (DeadlineTask) taskListVar.get(i+1);
+			if ((test.get(i+1) instanceof DeadlineTask) && !(test.get(i+1) instanceof TimedTask)){
+				DeadlineTask temp = (DeadlineTask) test.get(i+1);
 				DateTime tempDate = temp.getTime();
 				int tempDateDay = tempDate.getDayOfYear();
 				int tempDateYear = tempDate.getYear();
