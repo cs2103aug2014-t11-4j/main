2615aff375f8362dce15bb161e16a05e46ede142
sritampatnaik <sritampatnaik@gmail.com>
2014-10-27 14:16:08 +0800

    Minor changes

--- indigoSrc/InputWindow.java
+++ indigoSrc/InputWindow.java
@@ -229,7 +229,7 @@ public class InputWindow extends JFrame {
 
 		
 		if(componentIndex ==  TOP_PANEL_INDEX){
-			constraints = new GridBagConstraints(0,0,3,1,0.1,0.0,GridBagConstraints.CENTER,GridBagConstraints.BOTH,bottomPanel,0,0);
+			constraints = new GridBagConstraints(0,0,3,2,0.1,0.0,GridBagConstraints.CENTER,GridBagConstraints.BOTH,topPanel,0,0);
 
 			return constraints;
 		}
@@ -245,7 +245,7 @@ public class InputWindow extends JFrame {
 		
 
 		else if(componentIndex ==BOTTOM_PANEL_INDEX){
-			constraints = new GridBagConstraints(0,1,3,3,0.0,0.1,GridBagConstraints.CENTER,GridBagConstraints.BOTH,topPanel,0,0);
+			constraints = new GridBagConstraints(0,2,3,3,0.0,0.1,GridBagConstraints.CENTER,GridBagConstraints.BOTH,bottomPanel,0,0);
 			return constraints;
 		}
 		else if(componentIndex == TABBED_PANE_INDEX){
