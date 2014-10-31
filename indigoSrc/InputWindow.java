cfb979e40e5aae690970249819f6194b197832a8
jiajielu <lujiajie@nus.edu.sg>
2014-10-31 14:27:22 +0800

    icon fix

--- indigoSrc/InputWindow.java
+++ indigoSrc/InputWindow.java
@@ -39,7 +39,7 @@ public class InputWindow extends JFrame {
 		mainWindow.setBounds(300, 150, 700, 500);
 		mainWindow.setResizable(false);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainWindow.setIconImage(new ImageIcon("src/IndigoLogo.jpg").getImage());
+		mainWindow.setIconImage(new ImageIcon("gui/goindigologo.jpg").getImage());
 	}
 
 }
