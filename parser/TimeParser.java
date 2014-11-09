1f7cd134bb60d2a224253b03b6edcddba9ceddb6
joannakxt <joannakxt@ymail.com>
2014-11-09 23:00:42 +0800

    tagged class.

--- parser/TimeParser.java
+++ parser/TimeParser.java
@@ -1,5 +1,10 @@
 package parser;
 
+/** This class is meant to parse the command according to floating/deadline/timed task
+ * 	and filter out the identfiers if they are identified but are not an instance of time or date.
+ * @author Joanna
+ * 
+ */
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
